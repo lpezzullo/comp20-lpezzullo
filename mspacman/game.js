10a3cf87bb4217188db68a0c98f34fa1391f2dae
brickchip <lydia@pezzullo.com>
2015-03-01 16:27:08 -0500

    added event listener for img load

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,13 +1,16 @@
 function init() {
 	var canvas = document.getElementById('game_canvas');
-	if(canvas.getContext) {
+	if (canvas.getContext) {
 		var ctx = canvas.getContext('2d');
 		var img = new Image();
+		img.addEventListener("load", function() {
+  		// execute drawImage statements here
+  			ctx.drawImage(img, 332, 2, 464, 136, 0, 0, 464, 136);
+			ctx.drawImage(img, 83, 23, 13, 14, 35, 31, 13, 14);
+		}, false);
 		img.src = "pacman10-hp-sprite.png";
-		ctx.drawImage(img, 332, 2, 464, 136, 0, 0, 464, 136);
-		ctx.drawImage(img, 83, 23, 13, 14, 35, 31, 13, 14);
-	}
-	else {
+		
+	} else {
 		alert('Sorry - Ms Pac Man will not render on your browser.');
 	};
 };
