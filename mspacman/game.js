d4b29fbe4d210b5a6da8361b2145ea778b8db6fa
brickchip <lydia@pezzullo.com>
2015-03-01 15:06:56 -0500

    added load event listener & img source

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,5 +1,10 @@
 function draw() {
 	var canvas = document.getElementById('game_canvas');
 	var ctx = canvas.getContext('2d');
+	var img = new Image();
+	img.addEventListener("load",function() {
+		// drawImage statements
+	}, false);
+	img.src = "pacman10-hp-sprite.png";
 
 }
