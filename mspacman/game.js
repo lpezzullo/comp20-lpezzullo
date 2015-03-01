3eac6804af42fb430fc8a0778f36ec7a14e93833
brickchip <lydia@pezzullo.com>
2015-03-01 16:09:19 -0500

    code written but not displaying images

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,10 +1,20 @@
-function draw() {
+function init() {
 	var canvas = document.getElementById('game_canvas');
-	var ctx = canvas.getContext('2d');
-	var img = new Image();
-	img.addEventListener("load",function() {
-		// drawImage statements
-	}, false);
-	img.src = "pacman10-hp-sprite.png";
-
+	console.log('Got the canvas!');
+	if(canvas.getContext) {
+		console.log('We have context!');
+		ctx = canvas.getContext('2d');
+		console.log('Got the 2d context!');
+		var img = new Image();
+		console.log('Created Image object');
+		img.src = "pacman10-hp-sprite.png";
+		console.log('Sourced the image');
+		ctx.drawImage(img, 332, 2, 464, 136, 0, 0, 464, 136);
+		console.log('Executed drawImage on board');
+		ctx.drawImage(img, 83, 23, 13, 14, 35, 31, 13, 14);
+		console.log('Executed drawImage on Ms Pac Man');
+	}
+	else {
+		alert('Sorry - Ms Pac Man will not render on your browser.');
+	}
 }
