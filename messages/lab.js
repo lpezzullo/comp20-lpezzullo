80e3f5fbd8737101f621f49fabc4a8327f1b2fff
brickchip <lydia@pezzullo.com>
2015-03-07 11:57:20 -0500

    changed myCallbackFunction to parse

--- messages/lab.js
+++ messages/lab.js
@@ -3,10 +3,10 @@ xhr = new XMLHttpRequest();
 // Set up the request
 xhr.open("get", "https://github.com/tuftsdev/comp20-lpezzullo/messages/data.json", true);
 // Set up handler for the response
-xhr.onreadystatechange = myCallbackFunction;
+xhr.onreadystatechange = parse;
 // Execute the request
 xhr.send();
-function myCallbackFunction() {
+function parse() {
 	console.log("In my callback function " + xhr.readyState);
 	if (xhr.readyState == 4 && xhr.status == 200) {
 		alert("Got data back!");
