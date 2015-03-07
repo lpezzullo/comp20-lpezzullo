f2cd1e67ca2555980ceec28674a9fea265f3056b
brickchip <lydia@pezzullo.com>
2015-03-07 13:45:57 -0500

    apparently I have to commit these again

--- messages/lab.js
+++ messages/lab.js
@@ -1,22 +1,19 @@
+
+// Create instance of XHR object
+xhr = new XMLHttpRequest();
+// Set up the request
+xhr.open("get", "http://tuftsdev.github.io/comp20-lpezzullo/messages/data.json", true);
+// Set up handler for the response
+xhr.onreadystatechange = parse;
+// Execute the request
+xhr.send();
 function parse() {
-	// Create instance of XHR object
-	xhr = new XMLHttpRequest();
-	// Set up the request
-	xhr.open("get", "https://github.com/tuftsdev/comp20-lpezzullo/messages/data.json", true);
-	// Set up handler for the response
-	xhr.onreadystatechange = innerFcn;
-	// Execute the request
-	xhr.send();
-	function innerFcn() {
-		console.log("In my callback function " + xhr.readyState);
-		if (xhr.readyState == 4 && xhr.status == 200) {
-			alert("Got data back!");
-			data = JSON.parse(xhr.responseText);
-			/*var result = "";
-			for (i = 0; i < data.length; i++) {
-				result += "<p>" + data[i]['username'] + " - " + data[i]["content"] + "</p>";
-			}*/
-			document.getElementById("messages").innerHTML = xhr.responseText;
+	if (xhr.readyState == 4 && xhr.status == 200) {
+		data = JSON.parse(xhr.responseText);
+		var result = "";
+		for (i = 0; i < data.length; i++) {
+			result += "<p>" + data[i]["content"] + " - " +  data[i]['username'] + "</p>";
 		}
+		document.getElementById("messages").innerHTML = result;
 	}
 }
