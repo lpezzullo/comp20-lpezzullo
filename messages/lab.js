526498f718ef8a02601aa770dbd8c1e911653133
brickchip <lydia@pezzullo.com>
2015-03-07 11:51:20 -0500

    added basic xhr code

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,20 @@
-// Your JavaScript goes here...
+// Create instance of XHR object
+xhr = new XMLHttpRequest();
+// Set up the request
+xhr.open("get", "data.json", true);
+// Set up handler for the response
+xhr.onreadystatechange = myCallbackFunction;
+// Execute the request
+xhr.send();
+function myCallbackFunction() {
+	console.log("In my callback function " + xhr.readyState);
+	if (xhr.readyState == 4 && xhr.status == 200) {
+		alert("Got data back!");
+		data = JSON.parse(xhr.responseText);
+		/*var result = "";
+		for (i = 0; i < data.length; i++) {
+			result += "<p>" + data[i]['username'] + " - " + data[i]["content"] + "</p>";
+		}*/
+		document.getElementById("list").innerHTML = xhr.responseText;
+	}
+}
