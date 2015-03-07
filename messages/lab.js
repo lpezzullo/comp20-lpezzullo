627f2d0d2dc4fa28d35cc2b328e3fee079da8ff2
brickchip <lydia@pezzullo.com>
2015-03-07 12:06:33 -0500

    trying parse() as outer fcn

--- messages/lab.js
+++ messages/lab.js
@@ -1,20 +1,22 @@
-// Create instance of XHR object
-xhr = new XMLHttpRequest();
-// Set up the request
-xhr.open("get", "https://github.com/tuftsdev/comp20-lpezzullo/messages/data.json", true);
-// Set up handler for the response
-xhr.onreadystatechange = parse;
-// Execute the request
-xhr.send();
 function parse() {
-	console.log("In my callback function " + xhr.readyState);
-	if (xhr.readyState == 4 && xhr.status == 200) {
-		alert("Got data back!");
-		data = JSON.parse(xhr.responseText);
-		/*var result = "";
-		for (i = 0; i < data.length; i++) {
-			result += "<p>" + data[i]['username'] + " - " + data[i]["content"] + "</p>";
-		}*/
-		document.getElementById("messages").innerHTML = xhr.responseText;
+	// Create instance of XHR object
+	xhr = new XMLHttpRequest();
+	// Set up the request
+	xhr.open("get", "https://github.com/tuftsdev/comp20-lpezzullo/messages/data.json", true);
+	// Set up handler for the response
+	xhr.onreadystatechange = innerFcn;
+	// Execute the request
+	xhr.send();
+	function innerFcn() {
+		console.log("In my callback function " + xhr.readyState);
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			alert("Got data back!");
+			data = JSON.parse(xhr.responseText);
+			/*var result = "";
+			for (i = 0; i < data.length; i++) {
+				result += "<p>" + data[i]['username'] + " - " + data[i]["content"] + "</p>";
+			}*/
+			document.getElementById("messages").innerHTML = xhr.responseText;
+		}
 	}
 }
