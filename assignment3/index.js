631fbe921620fa6de8f1e65cf1ef3dac6eb700eb
brickchip <lydia@pezzullo.com>
2015-04-14 16:41:13 -0400

    added Ming's bodyParser init to fix TypeError

--- assignment3/index.js
+++ assignment3/index.js
@@ -1,5 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var validator = require('validator'); // See documentation at https://github.com/chriso/validator.js
 var app = express();
+// See https://stackoverflow.com/questions/5710358/how-to-get-post-query-in-express-node-js
+app.use(bodyParser.json());
+// See https://stackoverflow.com/questions/25471856/express-throws-error-as-body-parser-deprecated-undefined-extended
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -15,6 +21,7 @@ app.post('/whatup', function(request,response) {
 });
 
 app.get('/whatup', function(request,response) {
+	response.set('Content-Type', 'text/html');
 	var indexPage = "";
 	indexPage += "<!DOCTYPE HTML><html><head><title>what up</title></head><body><h1>whats up friendo</h1><p>";
 	indexPage += heresWhatsUp;
