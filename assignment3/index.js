617a7c25102ad50a53a4972a0191704a1023eb73
brickchip <lydia@pezzullo.com>
2015-04-14 11:56:26 -0400

    testing posting w curl & interacting w post

--- assignment3/index.js
+++ assignment3/index.js
@@ -4,10 +4,24 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+var heresWhatsUp = "";
+
 app.get('/', function(request, response) {
   response.send("It's working!");
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+app.post('/whatup', function(request,response) {
+	heresWhatsUp = request.body.message;
+});
+
+app.get('/whatup', function(request,response) {
+	var indexPage = "";
+	indexPage += "<!DOCTYPE HTML><html><head><title>what up</title></head><body><h1>whats up friendo</h1><p>";
+	indexPage += heresWhatsUp;
+	indexPage += "</p></body></html>"
+	response.send(indexPage);
+});
+	
+	app.listen(app.get('port'), function() {
+	"  console.log("Node app is running at localhost:" + app.get('port'));
 });
