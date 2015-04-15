4768355b8c222052940eda776ff76e3952630f7d
brickchip <lydia@pezzullo.com>
2015-04-15 19:52:01 -0400

    got locations db and home GET api working

--- assignment3/index.js
+++ assignment3/index.js
@@ -10,31 +10,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/test';
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/loctest';
 var MongoClient = require('mongodb').MongoClient, format = require('util').format;
 var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
 	db = databaseConnection;
 });
 
-var heresWhatsUp = "empty";
-
+/*	
 app.get('/', function(request, response) {
   response.send("It's working!");
 });
 
-app.post('/whatup', function(request,response) {
-	heresWhatsUp = request.body.message;
-	response.send("thats cool");
-});
-
-app.post('/addpasta', function(request, response) {
+app.post('/sendLocation', function(request, response) {
 	var noodle = request.body.noodle;
 	var toInsert = {
 		"noodle": noodle,
 	};
 	console.log("toInsert = " + toInsert.noodle);
 	db.collection('pasta', function(error1, coll) {
-		var id = coll.insert(toInsert, function(error2, saved) {
+		var id = coll.toInsertt(toInsert, function(error2, saved) {
 			if (error2) {
 				response.send(500);
 			}
@@ -46,34 +40,28 @@ app.post('/addpasta', function(request, response) {
 	});
 });
 
-app.get('/list', function(request, response) {
+*/
+app.get('/', function(request, response) {
+	// User types in: /location.json?login=<LOGIN>
+	// To access login sent by user: req.query.login
 	response.set('Content-Type', 'text/html');
 	var indexPage = '';
-	db.collection('pasta', function(er, collection) {
+	db.collection('locations', function(er, collection) {
 		collection.find().toArray(function(err, cursor) {
 			if (!err) {
-				indexPage += "<!DOCTYPE HTML><html><head><title>the pasta list</title></head><body><h1>mamma mia. that's a spicy meatball</h1>";
+				indexPage += "<!DOCTYPE HTML><html><head><title>Marauder Locations</title></head><body><h1>where is everbody?</h1>";
 				for (var count = 0; count < cursor.length; count++) {
-					indexPage += "<p>there's pasta called " + cursor[count].noodle + "!</p>";
+					indexPage += "<p>" + cursor[count].login + " last logged in from " + cursor[count].lat + ", " + cursor[count].lng + " on " + cursor[count].created_at + "</p>";
 				}
 				indexPage += "</body></html>"
 				response.send(indexPage);
 			} else {
-				response.send('<!DOCTYPE HTML><html><head><title>the pasta list</title></head><body><h1>Whoops, something went terribly wrong!</h1></body></html>');
+				response.send('<!DOCTYPE HTML><html><head><title>Marauder Locations</title></head><body><h1>Whoops, something went terribly wrong!</h1></body></html>');
 			}
 		});
 	});
 });
 
-app.get('/whatup', function(request,response) {
-	heresWhatsUp = "you called the get function!";
-	response.set('Content-Type', 'text/html');
-	var indexPage = "";
-	indexPage += "<!DOCTYPE HTML><html><head><title>what up</title></head><body><h1>whats up friendo</h1><p>";
-	indexPage += heresWhatsUp;
-	indexPage += "</p></body></html>"
-	response.send(indexPage);
-});
 	
 	app.listen(app.get('port'), function() {
 	console.log("Node app is running at localhost:" + app.get('port'));
